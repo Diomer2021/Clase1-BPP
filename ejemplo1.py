d0aa6723f62f8a5878e85a0d57461bd7aba99fdb
Diomer2021 <85360619+Diomer2021@users.noreply.github.com>
2022-03-05 13:14:17 -0500

    Correccion del error ejemplo1

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,4 +1,4 @@
 
 # Error de sintaxis 
-# sale error de sintaxis ya que luego de if no tiene los(:)
-if(5>2) print("El primer operador es mayor")
+# No sale error de sintaxis ya que luego de if  tiene los(:)
+if(5>2): print("El primer operador es mayor")
