b9140addfca0871f122a3846e94322afb5943a0c
Diomer2021 <85360619+Diomer2021@users.noreply.github.com>
2022-03-05 13:17:26 -0500

    correccion del ejemplo2

--- ejemplo2.py
+++ ejemplo2.py
@@ -1,13 +1,6 @@
 
-# Error de sintaxis
-# nos bota un errro de sintaxis en vez de g va f
-a = 5
-b = 3
-print(g"{a} es mayor que {b}")
-
 # El codigo correcto es:
-''' 
 a = 5
 b = 3
 print(f"{a} es mayor que {b}")
-'''
+
